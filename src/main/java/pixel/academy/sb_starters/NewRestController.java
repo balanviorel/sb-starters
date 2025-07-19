58ceb4da4600acfe1cec074e3d9c09030d6a55a6
balanviorel <balanviorel2@gmail.com>
2025-07-19 10:04:42 +0300

    Actuator added

--- src/main/java/pixel/academy/sb_starters/NewRestController.java
+++ src/main/java/pixel/academy/sb_starters/NewRestController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NewRestController {
 
-    @GetMapping
-    public String HelloTeam() {
-    return  "Hello World";
-}
+    @GetMapping("/exemplu")
+    public String exemplu() {
+        return  "Acesta este un exemplu";
+    }
 
 }
